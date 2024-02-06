00203e3420d90beda5a1178cf1752612df429a9a
Renukadevi Ananthan <83960274+Renukadeviananthan14@users.noreply.github.com>
2024-02-06 15:58:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import NavBar from './components/Navbar';
 import MyCarousel from './components/Carousel';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
@@ -14,8 +13,6 @@ function App() {
       <MyCarousel />
       <Skills />
       <Projects />
-      
-      <Contact />
       <Footer />
     </div>
   );
