9b878a05d8356cfef94d5a2e2a511211a7590d24
Renukadevi Ananthan <83960274+Renukadeviananthan14@users.noreply.github.com>
2024-02-06 15:59:26 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,12 +8,12 @@ import Atom from './atom.png';
 import Bootstrap from './bootstrap.png';
 import python from './icons8-python-480.png';
 import flask from './icons8-flask-250.png';
-import linux from './icons8-linux-96.png';
+import tail from './tailwind.png';
 import github from './icons8-github-480.png';
-import centos from './icons8-centos-96.png';
-import virtualbox from './icons8-virtualbox-logo-96.png';
+import electron from './electron.png';
 import wordpress from './icons8-wordpress-240.png';
 import Figma from './icons8-figma-240.png';
+import git from './git icon.png';
 import Nxt from './nxt.jpg';
 
 const Skills = () => {
@@ -36,11 +36,11 @@ const Skills = () => {
     <div class="col-6 col-sm-3"><img src={python} height="70px" width="70px"></img></div>
     <div class="col-6 col-sm-3"><img src={flask} height="70px" width="70px"></img></div>
     <div class="col-6 col-sm-3"><img src={github} height="70px" width="70px"></img></div>
-    <div class="col-6 col-sm-3"><img src={linux} height="70px" width="70px"></img></div>
+    <div class="col-6 col-sm-3"><img src={tail} height="70px" width="70px"></img></div>
     <div class="w-100"></div>
 
-    <div class="col-6 col-sm-3"><img src={centos} height="70px" width="70px"/></div>
-    <div class="col-6 col-sm-3"><img src={virtualbox} height="70px" width="70px"/></div>
+    <div class="col-6 col-sm-3"><img src={git} height="70px" width="70px"/></div>
+    <div class="col-6 col-sm-3"><img src={electron} height="70px" width="70px"/></div>
     <div class="col-6 col-sm-3"><img src={Atom} height="70px" width="70px"/></div>
     <div class="col-6 col-sm-3"><img src={Figma} height="70px" width="70px"/></div>
   </div>
